33b9bc453863af033bf0bab412f2722e6c5d5c0a
ultraspicy <73217673+ultraspicy@users.noreply.github.com>
2023-05-21 18:53:44 -0700

    Minor code refactor

--- programs/hello_world_anchor/src/lib.rs
+++ programs/hello_world_anchor/src/lib.rs
@@ -7,6 +7,21 @@ pub mod hello_world_anchor {
     use super::*;
 
     /**
+     * Example to initialize an counter which can be add_one'd by any user
+     * Zero input, just provide the most basic code for a stateful program
+     */
+    pub fn initialize_counter(ctx: Context<InitializeCounter>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        counter.count = 0;
+        Ok(())
+    }
+    pub fn add_one(ctx: Context<AddOne>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        counter.count = counter.count + 1;
+        Ok(())
+    }
+
+    /**
      * Example to initialize an account to store program data and update that data
      */
     pub fn initialize(ctx: Context<Initialize>, data: u64) -> Result<()> {
@@ -20,20 +35,32 @@ pub mod hello_world_anchor {
         Ok(())
     }
 
-    /**
-     * Example to initialize an counter which can be add_one'd by any user
-     */
-    pub fn initialize_counter(ctx: Context<InitializeCounter>) -> Result<()> {
-        let counter = &mut ctx.accounts.counter;
-        counter.count = 0;
-        Ok(())
-    }
-    pub fn add_one(ctx: Context<AddOne>) -> Result<()> {
-        let counter = &mut ctx.accounts.counter;
-        counter.count = counter.count + 1;
-        Ok(())
-    }
+}
 
+#[derive(Accounts)]
+pub struct InitializeCounter<'info> {
+    #[account(
+    init,
+    payer = authority,
+    space = 8 + 8,
+    )]
+    pub counter: Account<'info, Counter>,
+
+    #[account(mut)]
+    pub authority: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct AddOne<'info> {
+    #[account(mut)]
+    pub counter: Account<'info, Counter>,
+}
+
+#[account]
+pub struct Counter {
+    pub count: u64,
 }
 
 #[derive(Accounts)]
@@ -52,21 +79,6 @@ pub struct Initialize<'info> {
 }
 
 #[derive(Accounts)]
-pub struct InitializeCounter<'info> {
-    #[account(
-        init,
-        payer = authority,
-        space = 8 + 8,
-    )]
-    pub counter: Account<'info, Counter>,
-
-    #[account(mut)]
-    pub authority: Signer<'info>,
-
-    pub system_program: Program<'info, System>,
-}
-
-#[derive(Accounts)]
 pub struct Update<'info> {
     #[account(mut)]
     pub my_account: Account<'info, MyAccount>,
@@ -75,15 +87,4 @@ pub struct Update<'info> {
 #[account]
 pub struct MyAccount {
     pub data: u64,
-}
-
-#[derive(Accounts)]
-pub struct AddOne<'info> {
-    #[account(mut)]
-    pub counter: Account<'info, Counter>,
-}
-
-#[account]
-pub struct Counter {
-    pub count: u64,
 }
